520aa73e9f6a8b525bfd8663ed5ef90d582714d2
PeterAkande <akandepeter86@gmail.com>
2023-02-17 23:40:34 +0100

    Add documentation and Licence

--- lib/src/aws_storage_service/aws_upload_service/multipart_upload/isolate_message_config.dart
+++ lib/src/aws_storage_service/aws_upload_service/multipart_upload/isolate_message_config.dart
@@ -2,11 +2,15 @@ import 'dart:isolate';
 
 import '../upload_utils/create_chunk_size_config.dart';
 
+///[IsolateMessage] contains the needed variables for the isolate spawned when creating the file chunks for this file
 class IsolateMessage {
   //This class would house the isolate send port and the create chunk config to be used to
   //Split the file into chunks
 
-  SendPort sendPort;
+  SendPort
+      sendPort; // The sendport to send data to the isolate that has een spawned
+
+  ///[CreateChunkSizesIndexesConfig] contains information that determines how the chunks should be calculated
   CreateChunkSizesIndexesConfig config;
 
   IsolateMessage(this.config, this.sendPort);
